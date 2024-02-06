d0af7dabbeaaab35a439cee27fdccf3240e18acf
Souleimenblh <145621295+Souleimenblh@users.noreply.github.com>
2024-02-06 17:23:00 +0100

    first commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,27 @@ module.exports = {
   root: true,
   extends: ['plugin:@next/next/recommended', '@payloadcms'],
   ignorePatterns: ['**/payload-types.ts'],
-  plugins: ['prettier'],
+  plugins: ['prettier', 'import'],
+  ignorePatterns: [
+    '**/payload-types.ts',
+    '**/_providers/Auth',
+    '**/Button',
+    '**/CartLink',
+    '**/Link',
+  ],
+  rules: {
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    'no-console': 'off',
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
 }
